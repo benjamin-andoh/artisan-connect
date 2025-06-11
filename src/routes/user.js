6f4df3bdcaed67a723feccab9d1f2f6642ee147f
benoo.andoh <benoo.andoh@gmail.com>
2025-06-10 22:59:39 -0400

    add login functionality

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const User = require('../models/Users')
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const generateToken = require('../utils/jwt');
 
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-    //   const user = await User.create(req.body);
+      const user = await User.create(req.body);
       console.log("i am here ")
       res.status(201).json(user);
     } catch (error) {
@@ -35,6 +36,29 @@ router.post('/register', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+// ✅ User Login
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Check if user exists
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    // Compare passwords
+    // const isMatch = await bcrypt.compare(password, user.password);
+    // if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
+
+    // Generate token
+    const token = generateToken(user);
+
+    return res.json({ message: 'Login successful', token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
